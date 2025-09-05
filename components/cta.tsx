681f5645d0f1b41a7f98374640713c2bf8a8748f
lucaspanzera1 <lucassouzapanzera@gmail.com>
2025-09-05 10:54:07 -0300

    fix: contact hrefs

--- components/cta.tsx
+++ components/cta.tsx
@@ -29,7 +29,8 @@ export default function Cta() {
               <div data-aos="fade-up" data-aos-delay={400}>
                 <a
                   className="btn group mb-4 w-full bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                  href="#0"
+                  href="/budget"
+                target="_blank"
                 >
                   <span className="relative inline-flex items-center">
                     Solicitar Orçamento
@@ -42,7 +43,8 @@ export default function Cta() {
               <div data-aos="fade-up" data-aos-delay={600}>
                 <a
                   className="btn relative w-full bg-linear-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
-                  href="#0"
+                  href="https://api.whatsapp.com/send/?phone=553193713160"
+                  target="_blank"
                 >
                   Agendar Conversa
                 </a>
--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -40,7 +40,8 @@ export default function HeroHome() {
                 <div data-aos="fade-up" data-aos-delay={600}>
                   <a
                     className="btn relative w-full bg-linear-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
-                    href="#0"
+                    href="https://api.whatsapp.com/send/?phone=553193713160"
+                    target="_blank"
                   >
                     Fale conosco
                   </a>
